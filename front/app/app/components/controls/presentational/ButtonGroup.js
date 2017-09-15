87d84c94971fd8e6f87dd4409155e0f82b04658b
Matt Perkins <mperkins@redhat.com>
2017-09-15 17:03:48 -0400

    Comments

--- front/app/app/components/controls/presentational/ButtonGroup.js
+++ front/app/app/components/controls/presentational/ButtonGroup.js
@@ -5,10 +5,7 @@ import { withBootStrap, generateClassName } from '../shared/BootStrapHOC';
 export const ButtonToolBar = ({className='', ...rest}) =>
   <div role='toolbar' className={`btn-toolbar ${className}`} {...rest} />;
 
-// TODO
-// stacked btn-group-vertical
-// Nesting?
-// Note, sm and lg don't appear correctly when inside of a toolbar
+// TODO Note, sm and lg don't appear correctly when inside of a toolbar
 const BButtonGroup = (props) => {
   const El = styled.div.attrs({className: generateClassName(props)})``;
   return <El role='group'/>;
