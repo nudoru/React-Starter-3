9dbfd5d196e210a2b725e98dc3bf4cfc8c105704
Matt Perkins <mperkins@redhat.com>
2017-09-18 18:15:46 -0400

    animation comp

--- front/app/app/pages/Animations.js
+++ front/app/app/pages/Animations.js
@@ -15,6 +15,39 @@ const Container = styled.div`
   background-color: #eee;
 `;
 
+class Animatable extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.els = [];
+    this.anim = null;
+  }
+
+  componentDidMount() {
+    if (this.props.go) {
+      console.log('ANIMATION GO!');
+      const domEls = this.els.map(ReactDOM.findDOMNode);
+      this.anim = TweenMax.staggerTo(domEls, 0.5, { delay: 0.25, x: 500, yoyo: true, repeat: -1, ease: Expo.easeOut }, 0.75);
+    } else {
+      console.log('no animation')
+    }
+  }
+
+  render() {
+    const { children, go, ...rest } = this.props;
+
+    const newKids = React.Children.map(children, (child, idx) => {
+      const el = React.cloneElement(child, { 
+        key: idx,
+        ref: el => this.els.push(el)
+       });
+      
+      return el;
+    });
+
+    return <div {...rest}>{newKids}</div>;
+  }
+}
+
 class Animations extends React.Component {
   constructor(props) {
     super(props);
@@ -23,41 +56,43 @@ class Animations extends React.Component {
   }
 
   _onMouseEnter = e => {
-    let els = this.animEls.map(child => ReactDOM.findDOMNode(child));
+    // let els = this.animEls.map(ReactDOM.findDOMNode);
 
-    console.log(React.Children.toArray(this.helloRef.children));
+    // console.log(this.helloRef);
 
-    if (this.anim) {
-      //this.anim.play();
-    } else {
-      // Single element
-      // this.anim = TweenMax.to(ReactDOM.findDOMNode(this.helloRef), 0.25, {
-      //   x: 500,
-      //   yoyo: true,
-      //   repeat: -1,
-      //   ease: Expo.easeOut
-      // });
-      // Stagger
-      this.anim = TweenMax.staggerTo(
-        els,
-        0.5,
-        {
-          delay: 0.25,
-          x: 500,
-          yoyo: true,
-          repeat: -1,
-          ease: Expo.easeOut
-        },
-        0.75
-      );
-    }
+    // //console.log(React.Children.toArray(this.helloRef.children));
+
+    // if (this.anim) {
+    //   //this.anim.play();
+    // } else {
+    //   // Single element
+    //   // this.anim = TweenMax.to(ReactDOM.findDOMNode(this.helloRef), 0.25, {
+    //   //   x: 500,
+    //   //   yoyo: true,
+    //   //   repeat: -1,
+    //   //   ease: Expo.easeOut
+    //   // });
+    //   // Stagger
+    //   this.anim = TweenMax.staggerTo(
+    //     els,
+    //     0.5,
+    //     {
+    //       delay: 0.25,
+    //       x: 500,
+    //       yoyo: true,
+    //       repeat: -1,
+    //       ease: Expo.easeOut
+    //     },
+    //     0.75
+    //   );
+    // }
   };
 
   _onMouseLeave = e => {
-    if (this.anim) {
-      console.log('anim? ', this.anim);
-      //this.anim.pause();
-    }
+    // if (this.anim) {
+    //   console.log('anim? ', this.anim);
+    //   //this.anim.pause();
+    // }
   };
 
   render() {
@@ -71,15 +106,23 @@ class Animations extends React.Component {
         >
           Move it!
         </Button>
-        <div className="pt-5" ref={hello => (this.helloRef = hello)}>
-          <h1 ref={h => this.animEls.push(h)}>One</h1>
-          <h2 ref={h => this.animEls.push(h)}>Two</h2>
-          <h3 ref={h => this.animEls.push(h)}>Three</h3>
-        </div>
+        <Animatable go className="pt-5">
+          <h1>One</h1>
+          <h2>Two</h2>
+          <h3>Three</h3>
+        </Animatable>
       </Container>
     );
   }
 }
+
+/*
+<div className="pt-5" ref={hello => (this.helloRef = hello)}>
+          <h1 ref={h => this.animEls.push(h)}>One</h1>
+          <h2 ref={h => this.animEls.push(h)}>Two</h2>
+          <h3 ref={h => this.animEls.push(h)}>Three</h3>
+        </div>
+*/
 
 Animations.defaultProps = {};
 Animations.propTypes = {};
