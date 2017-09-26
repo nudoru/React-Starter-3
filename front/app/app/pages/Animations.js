4f07acf8da5badf713a97ea5d8e40d43a362a584
Matt Perkins <mperkins@redhat.com>
2017-09-26 14:53:14 -0400

    remove styled components

--- front/app/app/pages/Animations.js
+++ front/app/app/pages/Animations.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { TweenMax, Linear, Circ } from 'gsap';
-import styled from 'styled-components';
 import { range } from 'lodash';
 
 import { Animate, TweenGroup } from '../components/controls/shared/Animate';
@@ -20,44 +19,39 @@ import {
   CardFooter
 } from '../components/controls/presentational/Card';
 
-const Container = styled.div.attrs({ className: 'glass_water' })`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-`;
 
 class Animations extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
-    this.state = { anim: true, stop: false, counter: 5 };
+    this.state = {anim: true, stop: false, counter: 5};
   }
 
   _onPauseClick = e => {
-    this.setState(prevState => ({ anim: !prevState.anim }));
+    this.setState(prevState => ({anim: !prevState.anim}));
   };
 
   _onIncrementClick = e => {
-    this.setState(prevState => ({ counter: ++prevState.counter }));
+    this.setState(prevState => ({counter: ++prevState.counter}));
   };
 
   _onAddMoreClick = e => {
-    this.setState(prevState => ({ counter: (prevState.counter += 10) }));
+    this.setState(prevState => ({counter: (prevState.counter += 10)}));
   };
 
   _onDecrementClick = e => {
-    this.setState(prevState => ({ counter: --prevState.counter }));
+    this.setState(prevState => ({counter: --prevState.counter}));
   };
 
   _onRemoveClick = e => {
-    this.setState(prevState => ({ stop: true, counter: 0 }));
+    this.setState(prevState => ({stop: true, counter: 0}));
   };
 
-  _enterTweenFunc = ({ target, props, callBack }) => {
+  _enterTweenFunc = ({target, props, callBack}) => {
     return TweenMax.staggerFrom(
       target,
       1,
       {
-        x: 100,
+        x    : 100,
         alpha: 0
       },
       0.1,
@@ -65,33 +59,33 @@ class Animations extends React.Component {
     );
   };
 
-  _startTweenFunc = ({ target, props, callBack }) => {
+  _startTweenFunc = ({target, props, callBack}) => {
     console.log('start tween func');
     return TweenMax.set(target, {
       x: 50
     });
   };
 
-  _tweenFunc = ({ target, props, callBack }) => {
+  _tweenFunc = ({target, props, callBack}) => {
     return TweenMax.staggerTo(
       target,
       1,
       {
-        x: 500,
-        yoyo: true,
+        x     : 500,
+        yoyo  : true,
         repeat: -1,
-        ease: Circ.easeInOut
+        ease  : Circ.easeInOut
       },
       0.25
     );
   };
 
-  _leaveTweenFunc = ({ target, props, callBack }) => {
+  _leaveTweenFunc = ({target, props, callBack}) => {
     return TweenMax.staggerTo(
       target,
       0.5,
       {
-        y: 500,
+        y    : 500,
         alpha: 0
       },
       0.25,
@@ -99,9 +93,9 @@ class Animations extends React.Component {
     );
   };
 
-  render() {
+  render () {
     return (
-      <Container>
+      <div className='container-full glass_water'>
         <Button onClick={this._onPauseClick}>Pause</Button>
         <Button onClick={this._onIncrementClick}>Inc</Button>
         <Button onClick={this._onDecrementClick}>Dec</Button>
@@ -122,14 +116,13 @@ class Animations extends React.Component {
               </TweenGroup>
             );
           })}
-          </Animate>
-      </Container>
+        </Animate>
+      </div>
     );
   }
 }
 
-
 Animations.defaultProps = {};
-Animations.propTypes = {};
+Animations.propTypes    = {};
 
 export default Animations;
