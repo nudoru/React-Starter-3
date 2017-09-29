6b579068bc40a274b377e2284d1a95a7b04f9913
Matt Perkins <mperkins@redhat.com>
2017-09-29 10:36:16 -0400

    updated demo

--- front/app/app/pages/Components.js
+++ front/app/app/pages/Components.js
@@ -44,9 +44,25 @@ import {
 
 import Link from '../components/controls/interactive/Link';
 
+import { Flip, Face } from '../components/controls/interactive/Flip';
+
 import { withLoading } from '../components/controls/shared/IsLoadingHOC';
 
 const LoadingCard = withLoading(Card);
+
+const FlipFront = (props) => {
+  return (<div className='p-3'>
+    <h1>Front!</h1>
+    <Button primary onClick={props.flip}>Flip me</Button>
+  </div>)
+};
+
+const FlipBack = (props) => {
+  return (<div className='p-3'>
+    <h2>Back of the card</h2>
+    <Button light outline onClick={props.flip}>Flip me</Button>
+  </div>)
+};
 
 class Components extends React.Component {
 
@@ -57,6 +73,23 @@ class Components extends React.Component {
   render () {
     return (
       <div className="container mt-1">
+        <div className="pb-5">
+          <h3 className="pb-3">Flip card</h3>
+          <div className='p-5 flex-full-center risky_concrete'>
+          <Flip className='paper-shadow-lg mr-3' width={200} height={200}>
+            <Face className='bg-white'><FlipFront/></Face>
+            <Face className='bg-primary'><FlipBack/></Face>
+          </Flip>
+          <Flip className='paper-shadow-xl mr-3' width={300} height={300}>
+            <Face className='bg-white'><FlipFront/></Face>
+            <Face className='bg-danger'><FlipBack/></Face>
+          </Flip>
+          <Flip className='paper-shadow-sm mr-3' width={200} height={200}>
+            <Face className='bg-white'><FlipFront/></Face>
+            <Face className='bg-info'><FlipBack/></Face>
+          </Flip>
+          </div>
+        </div>
         <div className="pb-5">
           <h3 className="pb-3">Misc Elements</h3>
           <Link href='#' className='pr-2'>This is a link</Link>
